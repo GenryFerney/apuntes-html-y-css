c97080df686bd6d09ae33516017cc58adc6183bd
Genry Ferney Forero L <gfflfullstack@gmail.com>
2023-09-21 19:58:21 -0500

    Mi primer commit con todos los apuntes de html, curso backend

---
+++ listas.js
@@ -0,0 +1,37 @@
+/*let nombre = "Ferney";
+console.log(nombre);
+let edad = 25;
+console.log(edad);
+
+let amigos=["Ferney", "Juan","Fabio"]; 
+
+var objeto ={
+    color: "Black",
+     talla: 12
+}
+var edades =50;
+console.log(edades);
+edades ++;
+console.log(edades);
+var nuEstudiantes = 100;
+nuEstudiantes --;
+console.log(nuEstudiantes);
+nuEstudiantes = nuEstudiantes -1;
+console.log(nuEstudiantes);
+var a = 25;
+a+= 5;
+console.log(a);
+var totalVentas = 100000;
+console.log(totalVentas);
+totalVentas += 20000;
+console.log(totalVentas);*/
+ 
+var miArreglo = [1,2,3,4,5];
+//miArreglo.unshift(0);
+console.log(miArreglo);
+//miArreglo.shift(6);
+console.log(miArreglo);
+miArreglo.push(6);
+console.log(miArreglo);
+
+
